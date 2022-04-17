2955112585cba36cb8e509019178f537277249ad
oopera <rileasur@outlook.de>
2022-04-17 17:56:06 +0200

    no clue actually

--- my-app/src/ProductComponents/ProductList.js
+++ my-app/src/ProductComponents/ProductList.js
@@ -43,7 +43,7 @@ export default function ProductList(props) {
                     {props.openedItem !== product._id  &&
                         product.Name.toLowerCase().includes(searchInput.toLowerCase()) && (
                         <div onClick={() => updateProduct(product._id)}>
-                        <Product className={'Product'}name={product.Name} description={product.Description} price={product.Price} availability={product.Availability} key={product._id}/>
+                        <Product className={'Product'} name={product.Name} description={product.Description} price={product.Price} availability={product.Availability} key={product._id}/>
                         </div>
                             )}
                     {props.openedItem === product._id && (
