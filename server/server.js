197afe321c482233ba9a27768fd5943a5ec1fe04
oopera <rileasur@outlook.de>
2022-07-14 12:51:50 +0200

    CORS CORS CORS

--- server/server.js
+++ server/server.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors')
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+app.use(cors())
 app.use(express.json());
 app.use(require("./routes/routes"));
 // get driver connection
